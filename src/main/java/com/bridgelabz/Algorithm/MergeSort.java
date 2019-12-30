5b489184e5801cb0372d29754f253a941ef9c2aa
srijansalian <srijansalian@gmail.com>
2019-12-30 19:12:55 +0530

    [Srijan] MergeSort commented

--- src/main/java/com/bridgelabz/Algorithm/MergeSort.java
+++ src/main/java/com/bridgelabz/Algorithm/MergeSort.java
@@ -8,8 +8,8 @@ public class MergeSort {
 public static void main(String[] args) {
 	String a[]=util.inputString();
 	int s=0;
-	int e=a.length-1;
-	util.sort(a, s, e);
+	int e=a.length-1;/*Length*/
+	util.sort(a, s, e);/*Merge Sort operation of a string*/
 	
 	
 }
