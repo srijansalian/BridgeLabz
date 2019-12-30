7ab441eb6018b2371c95f7e57faadc7111406b71
srijansalian <srijansalian@gmail.com>
2019-12-30 19:10:12 +0530

    [Srijan] Findthenumber commented

--- src/main/java/com/bridgelabz/Algorithm/Findthenumber.java
+++ src/main/java/com/bridgelabz/Algorithm/Findthenumber.java
@@ -8,7 +8,7 @@ public class Findthenumber {
 	int k=util.inputInteger();
 	int n=(int )Math.pow(2, k);
 	System.out.println("Think of a number between 0 and "+n);
-	int a=util.search(0,n);
+	int a=util.search(0,n);/*Searching Operation a number is taken place*/  
 	System.out.println("Your number is "+a);
 	
 
