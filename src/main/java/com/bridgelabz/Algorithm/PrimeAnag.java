5a892e57461b870a724c187d4d4d37bbe6d24fd2
srijansalian <srijansalian@gmail.com>
2019-12-30 19:15:20 +0530

    [Srijan] PrimeAnag commented

--- src/main/java/com/bridgelabz/Algorithm/PrimeAnag.java
+++ src/main/java/com/bridgelabz/Algorithm/PrimeAnag.java
@@ -8,11 +8,11 @@ public class PrimeAnag {
 		 
 			
 			for (int i = 0; i <= 1000; i++) {
-				if (util.isPrime(i)) {
+				if (util.isPrime(i)) {/*Used to find out the given String is a Prime*/
 					for (int j = i; j < 1000; j++) {
 						if (i != j) {
-							if (util.isPrime(j)) {
-								if (util.anagram(String.valueOf(i), String.valueOf(j)) && util.isPalindrome(i)) {
+							if (util.isPrime(j)) { /*Used to find out the given String is a Prime*/
+								if (util.anagram(String.valueOf(i), String.valueOf(j)) && util.isPalindrome(i)) {/*Used to find out the anagram and palindrome*/
 									System.out.println(i + " " + j  );
 								}
 							}
