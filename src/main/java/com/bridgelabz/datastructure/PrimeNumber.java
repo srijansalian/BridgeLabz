f6573f59e248817057454d8564474fae2f07ad1f
srijansalian <srijansalian@gmail.com>
2020-01-02 14:34:15 +0530

    [Srijan].Implemented.Primerange modifed in com.bridgelabz.datastructure

--- src/main/java/com/bridgelabz/datastructure/PrimeNumber.java
+++ src/main/java/com/bridgelabz/datastructure/PrimeNumber.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.bridgelabz.util.DataStructureUtility;
 
+
 public class PrimeNumber {
 
 	public static ArrayList<Integer> prime() {
@@ -26,8 +27,8 @@ public class PrimeNumber {
 		// TODO Auto-generated method stub
 		al=prime();
 		int size=al.size();
-		//System.out.println(al);
-		//System.out.println(size);
+		System.out.println(al);
+		System.out.println(size);
 		primeDisplay(al,size);
 	}
 
@@ -36,18 +37,55 @@ public class PrimeNumber {
 
 	private static void primeDisplay(ArrayList<Integer> al, int size) 
 	{ 
-		int a[][]=new int[10][30];
-		for(int i=0;i<=10;i++) {
-			for(int j=0;j<=30;j++) {
-				a[i][j]=al.get(j);
+		int a[][]=new int[10][168];
+		for(int i=0;i<a.length;i++) 
+		{
+			for(int j=0;j<168 ;j++) 
+			{
+				if(i==0&&(al.get(j)>0&&al.get(j)<=100)) 
+					a[i][j] = al.get(j);
+					
+				
+				if(i==1&&(al.get(j)>100&&al.get(j)<=200)) 
+					a[i][j] = al.get(j);
+				if(i==2&&(al.get(j)>200&&al.get(j)<=300)) 
+					a[i][j] = al.get(j);
+				if(i==3&&al.get(j)>300&&al.get(j)<=400) 
+					a[i][j] = al.get(j);
+				if(i==4&&al.get(j)>400&&al.get(j)<=500) 
+					a[i][j] = al.get(j);
+				if(i==5&&al.get(j)>500&&al.get(j)<=600) 
+					a[i][j] = al.get(j);
+				if(i==6&&al.get(j)>600&&al.get(j)<=700) 
+					a[i][j] = al.get(j);
+				if(i==7&&al.get(j)>700&&al.get(j)<=800) 
+					a[i][j] = al.get(j);
+				if(i==8&&al.get(j)>800&&al.get(j)<=900) 
+					a[i][j] = al.get(j);
+				if(i==9&&al.get(j)>900&&al.get(j)<=1000) 
+					a[i][j] = al.get(j);
+				
 			}
-			
-			
-			
 		}
-		System.out.println(a);
-		// TODO Auto-generated method stub
+		for(int i1=0;i1<a.length;i1++) 
+		{
+			for(int j1=0;j1<a[i1].length;j1++) 
+			{
+				if(a[i1][j1]!=0) 
+				{
+					System.out.print(a[i1][j1]+" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		
 		
 	}
+
+
+
+
+	
 
 }
