7ce367c17324135ab2e2140afb0033fe26ab37dd
srijansalian <srijansalian@gmail.com>
2020-01-04 09:41:58 +0530

    [Srijan].Modification.SingleLinked list in com.bridgelabz.datastructure

--- src/main/java/com/bridgelabz/datastructure/SinglyLinkedList.java
+++ src/main/java/com/bridgelabz/datastructure/SinglyLinkedList.java
@@ -1,25 +1,26 @@
 package com.bridgelabz.datastructure;
 
-public class SinglyLinkedList {
-	class Node{
-		Object data;
-		Node next;
-		Node(Object data){
-			this.data=data;
+public class SinglyLinkedList<T> {
+	@SuppressWarnings("hiding")
+	class Node<T>{
+		T data;
+		Node<T>  next;
+		Node(T data){
+			this.data=(T) data;
 		}
 		
 	}
-	Node head;
+	Node<T> head;
 
-public boolean add(Object data)
+public boolean add(T data)
 {
-	Node n=new Node(data);
+	Node<T> n=new Node<T>(data);
 	if(head==null)
 	{
 		head=n;
 		return true;
 	}
-	Node t=head;
+	Node<T> t=head;
 	while(t.next!=null)
 	{
 		t=t.next;
@@ -29,7 +30,7 @@ public boolean add(Object data)
 }
 public String toString() {
 	String st="[";
-	Node t=head;
+	Node<T> t=head;
 	while(t!=null)
 	{
 		st=st+t.data;
@@ -40,32 +41,32 @@ public String toString() {
 	}
 	return st + "]";
 }
-public boolean addfirst(Object data)
+public boolean addfirst(T data)
 {
-	Node n=new Node(data);
+	Node<T> n=new Node<T>(data);
 	n.next=head;
 	head=n;
 	return true;
 }
-public Object deletefirst() {
+public T deletefirst() {
 	if(head==null)
 	{
 		System.out.println("List is empty............");
 		return null;
 	}
-	Object data=head.data;
+	T data=head.data;
 	head=head.next;
-	return data;
+	return (T) data;
 }
 
-public Object deleteLast() {
+public T deleteLast() {
 	if(head==null)
 	{
 		System.out.println("List is empty.....");
 		return null;
 	}
-	Node t=head;
-	Node t1=head;
+	Node<T> t=head;
+	Node<T> t1=head;
 	while(t.next!=null)
 	{
 		t1=t;
