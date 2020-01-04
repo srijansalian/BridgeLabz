4a1b5f7fe564972091c4fffe698f54e70f162052
srijansalian <srijansalian@gmail.com>
2020-01-04 17:41:25 +0530

    [Srijan].Changed methods. in Banking problem in datastructure package

---
+++ src/main/java/com/bridgelabz/datastructure/BankersProblem.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.datastructure;
+
+import com.bridgelabz.datastructure.Queue.Node;
+import com.bridgelabz.util.util;
+
+public class BankersProblem {
+
+	public static void main(String[] args) 
+	{
+		Queue<String> sk=new Queue<String>();
+		double amt1 = 10000;
+		System.out.println("Enter number of user");
+		int n = util.inputInteger();
+		for(int i=1;i<=n;i++) {
+			System.out.println("Enter the status of Transation W/D");
+			String a = util.inputWord();
+			System.out.println();
+			switch(a)
+			{
+			case "W":System.out.println("Enter the name of the user:");
+				     String user = util.inputWord();
+				     sk.enqueue(user);
+				     System.out.println("Enter the Amount to withdraw");
+				     double uamt = util.inputDouble();
+				     amt1 = amt1 - uamt;
+				     System.out.println("The remaining amounts are"+amt1);
+				     break;
+			
+			case "D":System.out.println("Enter the name of the user:");
+			         String user2 = util.inputWord();
+			         sk.enqueue(user2);
+			         System.out.println("Enter the Amount To deposite");
+			         double u1amt = util.inputDouble();
+			         amt1=amt1+u1amt;
+			         System.out.println("The amount of the deposite is"+u1amt);
+			         break;
+			         
+			 default:System.out.println("Invalid Input....!!!!Enter Correctly");
+			 
+		}
+			System.out.println();
+			System.out.println(sk);
+			
+		}
+		for(int i=1;i<=n;i++) {
+			Node str=sk.dequeue();
+			System.out.println(str+" is deleted");
+			
+		}
+		
+		
+		
+
+	}
+
+}
