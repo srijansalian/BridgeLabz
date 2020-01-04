71b600ba2d82cce8f6ccdecc0b543e42f9d51113
srijansalian <srijansalian@gmail.com>
2020-01-04 11:30:40 +0530

    [Srijan].Modification.Fileread UnorderedList in com.bridgelabz.datastructure

---
+++ src/main/java/com/bridgelabz/datastructure/FilereadUnorderedList.java
@@ -0,0 +1,61 @@
+package com.bridgelabz.datastructure;
+import java.io.*;
+import java.util.Scanner;
+import com.bridgelabz.util.DataStructureUtility;
+
+public class FilereadUnorderedList<T> {
+
+	public static void main(String[] args) {
+		try {
+			
+		File f1 = new File("C:\\Users\\Srijan Salian\\Desktop\\1.txt");
+		SinglyLinkedList<String> list = new SinglyLinkedList<>();
+		
+			Scanner sf = new Scanner(f1);
+			Scanner s = new Scanner(System.in);
+			while (sf.hasNext()) {
+				list.add(sf.next());
+			}
+			// printing list
+			System.out.println(list);
+			/*
+			 * writing to file using file writer
+			 */
+			FileWriter fw = new FileWriter(f1);
+			System.out.println("enter a word ");
+			String s1 = s.nextLine();
+			System.out.println(list.size());
+			// checking if word is found and deleting it and then saving list to file
+			if (list.search(s1)) {
+				list.remove(s1);
+				int n = 0;
+				while (n < list.size()) {
+					fw.write(list.pop() + " ");
+					fw.flush();
+					n++;
+				}
+				System.out.println("found and deleted");
+				// or adding it to list and saving it to file
+			} else {
+				list.add(s1);
+				int n = 0;
+				while (n < list.size()) {
+					fw.write(list.pop() + " ");
+					fw.flush();
+					n++;
+				}
+				System.out.println("added and saved");
+				fw.close();
+				s.close();
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("file not found");
+		} catch (IOException e) {
+			System.out.println("io excep");
+		}
+	}
+		
+}
+
+
--- src/main/java/com/bridgelabz/datastructure/SinglyLinkedList.java
+++ src/main/java/com/bridgelabz/datastructure/SinglyLinkedList.java
@@ -1,7 +1,9 @@
 package com.bridgelabz.datastructure;
 
 public class SinglyLinkedList<T> {
+	int size=0;
 	@SuppressWarnings("hiding")
+	
 	class Node<T>{
 		T data;
 		Node<T>  next;
@@ -26,6 +28,7 @@ public boolean add(T data)
 		t=t.next;
 	}
 	t.next=n;
+	size+=1;
 	return true;
 }
 public String toString() {
@@ -46,6 +49,7 @@ public boolean addfirst(T data)
 	Node<T> n=new Node<T>(data);
 	n.next=head;
 	head=n;
+	size+=1;
 	return true;
 }
 public T deletefirst() {
@@ -56,6 +60,7 @@ public T deletefirst() {
 	}
 	T data=head.data;
 	head=head.next;
+	size-=1;
 	return (T) data;
 }
 
@@ -76,6 +81,73 @@ public T deleteLast() {
 		head=null;
 	else
 		t1.next=null;
+	size-=1;
 	return t.data;
+	
 }
+public void remove(T item) {
+	Node<T> n = head;
+	Node<T> prev = null;
+	if (head.data.equals(item)) {
+		head = head.next;
+		size -= 1;
+		return;
+	}
+	while (!n.data.equals(item)) {
+		prev = n;
+		n = n.next;
+	}
+	n = n.next;
+	prev.next = n;
+	n = null;
+	size -= 1;
+}
+public void removeAtLast() {
+	Node<T> n = head;
+	Node<T> prev = null;
+	while (n.next != null) {
+		prev = n;
+		n = n.next;
+	}
+	//System.out.println("hheeyy");
+	// remove the node as last node
+	prev.next = null;
+	size--;
+}
+public boolean search(T item) {
+	Node<T> n = head;
+	while (n.next != null) {
+		if (n.data.equals(item)) {
+			return true;
+		}
+		n = n.next;
+	}
+	return false;
+}
+public boolean isEmpty() {
+	if (head == null) {
+		return true;
+	}
+	return false;
+}
+public int size() {
+	return size;
+}
+public T pop() {
+	Node n = head;
+	if (size == 1) {
+		head = head.next;
+		size--;
+		return (T) n.data;
+	}
+	while (n.next != null) {
+		n = n.next;
+	}
+	T ret = (T) n.data;
+	n = n.next;
+	size -= 1;
+	return ret;
+}
+
+
 }
