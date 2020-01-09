5bf81a0d1714c3b0d6afa78fa4f4ea1056ec7d81
srijansalian <srijansalian@gmail.com>
2020-01-09 19:07:22 +0530

    [Srijan].Modification.in InventoryService on OOPS

--- src/main/java/com/bridgelabz/OOPS/Service/InventoryService.java
+++ src/main/java/com/bridgelabz/OOPS/Service/InventoryService.java
@@ -1,5 +1,33 @@
 package com.bridgelabz.OOPS.Service;
 
 public class InventoryService {
+	private String name;
+	private int weight;
+	private int weight_per_kg;
+	private int totalprize;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getWeight_per_kg() {
+		return weight_per_kg;
+	}
+	public void setWeight_per_kg(int weight_per_kg) {
+		this.weight_per_kg = weight_per_kg;
+	}
+	public int getTotalprize() {
+		return totalprize;
+	}
+	public void setTotalprize(int totalprize) {
+		this.totalprize = totalprize;
+	}
 
 }
