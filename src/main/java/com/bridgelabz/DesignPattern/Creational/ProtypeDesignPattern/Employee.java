e3c9cc22134a3de26cd4a68eeeaa752270878155
srijansalian <srijansalian@gmail.com>
2020-01-20 14:55:51 +0530

    [Srijan].Implemented.Employee in Protype Design Pattern

--- src/main/java/com/bridgelabz/DesignPattern/Creational/ProtypeDesignPattern/Employee.java
+++ src/main/java/com/bridgelabz/DesignPattern/Creational/ProtypeDesignPattern/Employee.java
@@ -1,5 +1,34 @@
 package com.bridgelabz.DesignPattern.Creational.ProtypeDesignPattern;
 
-public class Employee {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee implements Cloneable{
+	
+	private List<String> emplist;
+	
+	private Employee(List<String> temp) {
+		emplist = new ArrayList<String>();
+		
+	}
+	public void Load() {
+		emplist.add("Srijan");
+		emplist.add("Brijesh");
+		emplist.add("Nayan");
+		emplist.add("Pradeep");
+		emplist.add("Mahesh");
+	}
+	public List<String> getList(){
+		return emplist;
+	}
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		List<String> temp = new ArrayList<String>();
+		for(String a : this.getList()) {
+			temp.add(a);
+		}
+		return new Employee(temp);
+	}
+	
 
 }
