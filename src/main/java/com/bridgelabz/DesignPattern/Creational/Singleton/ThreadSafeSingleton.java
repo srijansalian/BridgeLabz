754610250850752ca409f5777bb2dff9b8fb5827
srijansalian <srijansalian@gmail.com>
2020-01-21 11:59:02 +0530

    [Srijan].Implemented.ThreadSafe Initialization in Design Pattern

---
+++ src/main/java/com/bridgelabz/DesignPattern/Creational/Singleton/ThreadSafeSingleton.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.DesignPattern.Creational.Singleton;
+
+public class ThreadSafeSingleton {
+	
+	private static ThreadSafeSingleton instance;
+	
+	private ThreadSafeSingleton() {}
+	
+	public static synchronized ThreadSafeSingleton getInstance() {
+		if(instance==null) {
+			instance = new ThreadSafeSingleton();
+		}
+		return instance;
+	}
+
+}
