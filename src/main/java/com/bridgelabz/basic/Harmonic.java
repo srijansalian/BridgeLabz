aa1111d05a7cdc7bc3dfffa8534c5396c69d4e54
srijansalian <srijansalian@gmail.com>
2020-01-22 18:09:37 +0530

    [Srijan].updated.in LeapYear in basics

--- src/main/java/com/bridgelabz/basic/Harmonic.java
+++ src/main/java/com/bridgelabz/basic/Harmonic.java
@@ -1,20 +1,13 @@
 package com.bridgelabz.basic;
+
 import com.bridgelabz.util.util;
+
 public class Harmonic {
-	public static double Harnoi(int n) {
-		float h1=1;
-		for(int i=2;i<=n;i++) {
-			h1+=(float)1/i;
-		}
-			return h1;
-		
-	}
 
 	public static void main(String[] args) {
 		System.out.println("Enter the value of n");
-		int n=util.inputInteger();
-		System.out.println(Harnoi(n));
-		
+		int n = util.inputInteger();
+		System.out.println(util.Harnoi(n));
 
 	}
 
--- src/main/java/com/bridgelabz/basic/LeapYear.java
+++ src/main/java/com/bridgelabz/basic/LeapYear.java
@@ -5,11 +5,11 @@ import com.bridgelabz.util.util;
 public class LeapYear {
 
 	public static void main(String[] args) {
-				System.out.println("Enter the year");
-		int year=util.inputInteger();
-		boolean l=util.isleap(year);
-		if(l)
-			System.out.println("The "+year+" is a leap year" );
+		System.out.println("Enter the year");
+		int year = util.inputInteger();
+		boolean l = util.isleap(year);
+		if (l)
+			System.out.println("The " + year + " is a leap year");
 		else
 			System.out.println("Not an leap year");
 
