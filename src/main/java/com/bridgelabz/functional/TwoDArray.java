2e375766d33a66532ee6e66e7214c8ec36c000b4
srijansalian <srijansalian@gmail.com>
2020-01-22 18:22:37 +0530

    [Srijan].updated.in TwoDArray in Functional

--- src/main/java/com/bridgelabz/functional/TwoDArray.java
+++ src/main/java/com/bridgelabz/functional/TwoDArray.java
@@ -8,13 +8,13 @@ import com.bridgelabz.util.util;
 public class TwoDArray {
 
 	public static void main(String[] args) {
-		
-		PrintWriter pw=new PrintWriter(new OutputStreamWriter(System.out));
-		int a[][]=util.TwodArray();
+
+		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+		int a[][] = util.TwodArray();
 		System.out.println("The values are ");
-		for(int i=0;i<a.length;i++) {
-			for(int j=0;j<a[i].length;j++) {
-				pw.println(a[i][j]+" ");
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				pw.println(a[i][j] + " ");
 			}
 		}
 		pw.flush();
