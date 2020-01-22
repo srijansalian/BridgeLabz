10f5079e9708f0a56f04ecb8de2a866c2201a4fe
srijansalian <srijansalian@gmail.com>
2020-01-22 19:04:08 +0530

    [Srijan].updated.on MergeSort in Algorithm

--- src/main/java/com/bridgelabz/algorithm/MergeSort.java
+++ src/main/java/com/bridgelabz/algorithm/MergeSort.java
@@ -1,16 +1,17 @@
+/*
+ * Merge Sort of the String
+ */
 package com.bridgelabz.algorithm;
 
 import com.bridgelabz.util.util;
 
 public class MergeSort {
-	
 
-public static void main(String[] args) {
-	String a[]=util.inputString();
-	int s=0;
-	int e=a.length-1;/*Length*/
-	util.sort(a, s, e);/*Merge Sort operation of a string*/
-	
-	
-}
+	public static void main(String[] args) {
+		String a[] = util.inputString();
+		int s = 0;
+		int e = a.length - 1;/* Length */
+		util.sort(a, s, e);/* Merge Sort operation of a string */
+
+	}
 }
