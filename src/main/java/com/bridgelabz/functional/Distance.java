2b4e57b3c7c5af2c7655e0ef8bcc8ff9bb1ecc6e
srijansalian <srijansalian@gmail.com>
2020-01-22 18:13:39 +0530

    [Srijan].updated.in Distance in Functional

--- src/main/java/com/bridgelabz/functional/Distance.java
+++ src/main/java/com/bridgelabz/functional/Distance.java
@@ -5,11 +5,11 @@ import com.bridgelabz.util.util;
 public class Distance {
 
 	public static void main(String[] args) {
-		int x=util.inputInteger();
-		int y=util.inputInteger();
-		int result=(x*x+y*y);
-		double d=Math.pow(result, 0.5);
+		int x = util.inputInteger();
+		int y = util.inputInteger();
+		int result = (x * x + y * y);
+		double d = Math.pow(result, 0.5);
 		System.out.println(d);
-		}
+	}
 
 }
