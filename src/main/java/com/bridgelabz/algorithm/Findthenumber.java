e0e62933e636d6a684c776995e7920469f4b0773
srijansalian <srijansalian@gmail.com>
2020-01-22 19:00:54 +0530

    [Srijan].updated.on Find the Number in Algorithm

--- src/main/java/com/bridgelabz/algorithm/Findthenumber.java
+++ src/main/java/com/bridgelabz/algorithm/Findthenumber.java
@@ -1,18 +1,20 @@
+/*
+ * To find out the guess the number from the user
+ */
 package com.bridgelabz.algorithm;
 
 import com.bridgelabz.util.util;
+
 public class Findthenumber {
 	public static void main(String[] args) {
-	System.out.println("Enter a number");
-	
-	int k=util.inputInteger();
-	int n=(int )Math.pow(2, k);
-	System.out.println("Think of a number between 0 and "+n);
-	int a=util.search(0,n);/*Searching Operation a number is taken place*/  
-	System.out.println("Your number is "+a);
-	
+		System.out.println("Enter a number");
 
-}
+		int k = util.inputInteger();
+		int n = (int) Math.pow(2, k);
+		System.out.println("Think of a number between 0 and " + n);
+		int a = util.search(0, n);/* Searching Operation a number is taken place */
+		System.out.println("Your number is " + a);
 
+	}
 
 }
