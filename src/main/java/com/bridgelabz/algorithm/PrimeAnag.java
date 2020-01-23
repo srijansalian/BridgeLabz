a655c14e697bcfc7450b50e9c709e7b265ab3878
srijansalian <srijansalian@gmail.com>
2020-01-23 18:55:42 +0530

    [Srijan].Updated.PrimeAnagram in Algorithm

--- src/main/java/com/bridgelabz/algorithm/PrimeAnag.java
+++ src/main/java/com/bridgelabz/algorithm/PrimeAnag.java
@@ -2,27 +2,27 @@ package com.bridgelabz.algorithm;
 
 import com.bridgelabz.util.util;
 
+/*
+ * To display the in the 2D array in the Prime and Anagram
+ */
 public class PrimeAnag {
 
 	public static void main(String[] args) {
-		 
-			
-			for (int i = 0; i <= 1000; i++) {
-				if (util.isPrime(i)) {/*Used to find out the given String is a Prime*/
-					for (int j = i; j < 1000; j++) {
-						if (i != j) {
-							if (util.isPrime(j)) { /*Used to find out the given String is a Prime*/
-								if (util.anagram(String.valueOf(i), String.valueOf(j)) && util.isPalindrome(i)) {/*Used to find out the anagram and palindrome*/
-									System.out.println(i + " " + j  );
-								}
-							}
-						}
+
+		for (int i = 0; i <= 1000; i++) {
+			if (util.isPrime(i)) {/* Used to find out the given String is a Prime */
+				for (int j = i; j < 1000; j++) {
+					if (i != j && util.isPrime(j) && util.anagram(String.valueOf(i), String.valueOf(j))
+							&& util.isPalindrome(i)) {
+
+						System.out.println(i + " " + j);
 					}
 
 				}
+			}
 
+		}
 
 	}
 
-	}
 }
