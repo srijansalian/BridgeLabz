75057c418dcf8565a19213ac1f5824fafcf144f8
srijansalian <srijansalian@gmail.com>
2020-01-23 11:26:34 +0530

    [Srijan].Implemented.HelperOracle in structural facade on Design Pattern

---
+++ src/main/java/com/bridgelabz/designpattern/structural/facadedesignpattern/MyOracle.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.designpattern.structural.facadedesignpattern;
+
+import java.sql.Connection;
+
+public class MyOracle {
+	public static Connection getOracleDBConnection() {
+		// get Oracle DB connection using connection parameters
+		return null;
+	}
+
+	public void generateOraclePDFReport(String tableName, Connection con) {
+		// get data from table and generate pdf report
+	}
+
+	public void generateOracleHTMLReport(String tableName, Connection con) {
+		// get data from table and generate pdf report
+	}
+
+}
