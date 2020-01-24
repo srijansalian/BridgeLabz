cd6a36ed381bbc883a05190f2a9bed6dde5ff0a2
srijansalian <srijansalian@gmail.com>
2020-01-24 15:15:28 +0530

    [Srijan]Implemented.Mediator design pattern in Design Pattern

--- src/main/java/com/bridgelabz/DesignPattern/behavioral/mediator/UserImp.java
+++ src/main/java/com/bridgelabz/DesignPattern/behavioral/mediator/UserImp.java
@@ -8,13 +8,13 @@ public class UserImp extends User {
 
 	@Override
 	public void send(String msg) {
-		System.out.println(this.name + ": Sending Message=" + msg);
+		System.out.println(this.name + ": Sending Message =" + msg);
 		mediator.sendMessage(msg, this);
 	}
 
 	@Override
 	public void receive(String msg) {
-		System.out.println(this.name + ": Received Message:" + msg);
+		System.out.println(this.name + ": Received Message :" + msg);
 	}
 
 }
