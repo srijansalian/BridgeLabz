7907bb05892e5f527648de20e1c574bc62c6ffc4
srijansalian <srijansalian@gmail.com>
2020-01-24 12:08:49 +0530

    [Srijan]Implemented.Visitor design pattern in Design Pattern

--- src/main/java/com/bridgelabz/DesignPattern/behavioral/visitor/VisitorTest.java
+++ src/main/java/com/bridgelabz/DesignPattern/behavioral/visitor/VisitorTest.java
@@ -3,26 +3,24 @@ package com.bridgelabz.DesignPattern.behavioral.visitor;
 public class VisitorTest {
 
 	public static void main(String[] args) {
-		
+
 		TaxVisitor Taxvistor = new TaxVisitor();
-		
+
 		TaxHoildayVisitor TaxHoilday = new TaxHoildayVisitor();
-		
+
 		Liquor Beer = new Liquor(195);
 		Tobacco cigar = new Tobacco(45);
 		Necessity juice = new Necessity(80);
-		
+
 		System.out.println(Beer.accept(Taxvistor) + "\n");
 		System.out.println(cigar.accept(Taxvistor) + "\n");
 		System.out.println(juice.accept(Taxvistor) + "\n");
-		
+
 		System.out.println("HOILDAY DISCOUNT !!!!!!!!! ");
-		
+
 		System.out.println(Beer.accept(TaxHoilday) + "\n");
 		System.out.println(cigar.accept(TaxHoilday) + "\n");
 		System.out.println(juice.accept(TaxHoilday) + "\n");
-		
-		
 
 	}
 
